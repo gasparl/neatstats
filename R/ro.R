e562010112cd22a0112adc4c1a0ec8a5be5e34d2
Gaspar <lkcsgaspar@gmail.com>
2019-06-08 10:59:21 +0200

    readme update

--- R/ro.R
+++ R/ro.R
@@ -1,9 +1,9 @@
 #' Neat rounding
 #'
 #' This function neatly rounds any number to given number of digits after the decimal point.
-#' @export
 #' @examples
 #' ro()
+#' @export
 ro = function(value, round_to = 2) {
     value = as.numeric( value )
     return(format(round(value, round_to), nsmall = round_to))
