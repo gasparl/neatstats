35c8bbf136340d160d8be57a5a9e683803ee2206
Gaspar Lukacs <lkcsgaspar@gmail.com>
2020-08-11 12:07:19 +0200

    enumerate added

---
+++ R/enum.R
@@ -0,0 +1,41 @@
+#' @title Enumerate
+#'
+#' @description Aids enumeration and merging (via \code{\link{rbind_loop}}) in
+#'   loops: adds numbers to a vector input, and indicates loop start for
+#'   \code{\link{rbind_loop}}.
+#' @param items The items to be enumerated in the loop.
+#' @param hush Logical. If \code{TRUE} (default), prints "Loop started." when
+#'   executed.
+#' @param enumerate Logical. If \code{TRUE} (default), adds numbering to the
+#'   input vector (in item pairs, see Examples). If \code{FALSE}, returns the
+#'   original input.
+#' @return Vector with numbers added (if so set).
+#' @seealso \code{\link{rbind_neat}}
+#' @examples
+#'
+#' my_vector = c('aa', 'bb', 'cxyz', 'last')
+#'
+#' for (item in enum(my_vector)) {
+#'     print(item)
+#' }
+#'
+#' # just to show what enum() returns
+#' enum(my_vector)
+#'
+#' @export
+enum = function(items,
+                hush = FALSE,
+                enumerate = TRUE) {
+    validate_args(match.call(),
+                  list(val_arg(hush, c('bool'), 1),
+                       val_arg(enumerate, c('bool'), 1)))
+    if (hush == FALSE) {
+        cat('Loop started.', fill = TRUE)
+    }
+    pkg.globals$my_unique_first_iter = TRUE
+    if (enumerate == TRUE) {
+        return(mapply(c, 1:length(items), items, SIMPLIFY = FALSE))
+    } else {
+        return(items)
+    }
+}
---
+++ R/rbind_neat.R
@@ -0,0 +1,47 @@
+#' @title Merge by Columns in Loops
+#'
+#' @description Merges raws by columns in a loop using the \code{\link{enum}}
+#'   function. On first iteration, indicated by \code{\link{enum}}, initiates a
+#'   new data frame with the data to merge as first row. On all following
+#'   iterations, adds data to merge as subsequent rows (using
+#'   \code{\link[plyr:rbind.fill]{plyr:rbind.fill}}).
+#'
+#' @param merged The name of the data frame for the merged data (without
+#'   quotes).
+#' @param to_merge Data frame to be merged. (Normally a single-row data frame.)
+#'@details See an extensive example via https://github.com/gasparl/neatstats.
+#' @seealso \code{\link{enum}}
+#' @examples
+#'
+#' my_vector = c('aa', 'bb', 'cxyz', 'last')
+#' for (elem in enum(my_vector)) {
+#'     cat(elem, fill = TRUE)
+#'     datfram = data.frame(item = elem[2],
+#'                          number = elem[1],
+#'                          whatever = paste0('number (', elem[1], ')'))
+#'     rbind_loop(merged_data, datfram)
+#' }
+#' # merged_data now contained all merged rows
+#' print(merged_data)
+#'
+#' # an extensive example to show how to collect and aggregate raw data is
+#' # available via the README file at the repository:
+#' # https://github.com/gasparl/neatstats
+#'
+#' @export
+rbind_loop = function(merged, to_merge) {
+    merged_str = deparse(substitute(merged))
+    if (is.null(pkg.globals$my_unique_first_iter)) {
+        stop(
+            'First iteration was not indicated via the neatStats::enum() function. ',
+            'Please use enum() if you want to use rbind_neat(). See ?rbind_neat for details'
+        )
+    } else if (pkg.globals$my_unique_first_iter  == TRUE) {
+        pkg.globals$my_unique_first_iter = FALSE
+        assign(merged_str, to_merge, envir = .GlobalEnv)
+    } else if (pkg.globals$my_unique_first_iter  == FALSE) {
+        assign(merged_str,
+               plyr::rbind.fill(merged, to_merge),
+               envir = .GlobalEnv)
+    }
+}
